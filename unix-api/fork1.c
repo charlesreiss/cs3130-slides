4e3f3aed07bab98b77d9b6bed0b55697302a0988
cr4bd <cr4bd@virginia.edu>
2023-09-17 21:59:05 -0400

    Picture for fork example

--- unix-api/fork1.c
+++ unix-api/fork1.c
@@ -1,7 +1,4 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/types.h>
+// not shown: #include various headers
 int main(int argc, char *argv[]) {
     pid_t pid = @2getpid()2@;
     printf("Parent pid: %d\n", @3(int)3@ pid);
@@ -9,11 +6,14 @@ int main(int argc, char *argv[]) {
     if (child_pid > 0) {
         /* Parent Process */
         pid_t my_pid = @2getpid()2@;
-        printf("[%d] parent of [%d]\n", @3(int)3@ my_pid, @3(int)3@ child_pid);
+        printf("[%d] parent of [%d]\n",
+               @3(int)3@ my_pid,
+               @3(int)3@ child_pid);
     } else if (child_pid == 0) {
         /* Child Process */
         pid_t my_pid = @2getpid()2@;
-        printf("[%d] child\n", @3(int)3@ my_pid);
+        printf("[%d] child\n",
+               @3(int)3@ my_pid);
     } else {
         @4perror4@("Fork failed");
     }
