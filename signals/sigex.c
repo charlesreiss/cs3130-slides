39581a1d96294c01e533751f00a996bf4f0f3c1b
Charles Reiss <creiss@virginia.edu>
2024-09-05 17:38:08 -0400

    adjust second API exercise to fix timing problem

--- signals/sigex.c
+++ signals/sigex.c
@@ -8,6 +8,8 @@ int main(void) {
     struct sigaction act;
     act.sa_handler = @3&handle_sigint@;
     sigemptyset(&act.sa_mask);
+    // SA_RESTART = if syscall interrupted,
+    // complete it when handler returns
     act.sa_flags = SA_RESTART;
     @2sigaction(SIGINT, &act, NULL);@
 
