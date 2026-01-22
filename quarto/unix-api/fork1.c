7388834ff99a1be16c51b3cf0af32ed1a52415a3
cr4bd <cr4bd@virginia.edu>
2026-01-21 22:22:49 -0500

    unix-api slide initial cleanup

--- quarto/unix-api/fork1.c
+++ quarto/unix-api/fork1.c
@@ -1,21 +1,21 @@
 // not shown: #include various headers
 int main(int argc, char *argv[]) {
-    pid_t pid = @2getpid()2@;
-    printf("Parent pid: %d\n", @3(int)3@ pid);
+    pid_t pid = getpid();
+    printf("Parent pid: %d\n", (int) pid);
     pid_t child_pid = fork();
     if (child_pid > 0) {
         /* Parent Process */
-        pid_t my_pid = @2getpid()2@;
+        pid_t my_pid = getpid();
         printf("[%d] parent of [%d]\n",
-               @3(int)3@ my_pid,
-               @3(int)3@ child_pid);
+               (int) my_pid,
+               (int) child_pid);
     } else if (child_pid == 0) {
         /* Child Process */
-        pid_t my_pid = @2getpid()2@;
+        pid_t my_pid = getpid();
         printf("[%d] child\n",
-               @3(int)3@ my_pid);
+               (int) my_pid);
     } else {
-        @4perror4@("Fork failed");
+        perror("Fork failed");
     }
     return 0;
 }
